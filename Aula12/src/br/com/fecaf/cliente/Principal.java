2c528274616a2c8c09c8aa2b0e0477c071f132db
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2022-12-23 16:57:50 -0200

    Atualização Aula12

--- Aula12/src/br/com/fecaf/cliente/Principal.java
+++ Aula12/src/br/com/fecaf/cliente/Principal.java
@@ -1,9 +1,38 @@
 package br.com.fecaf.cliente;
 
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import br.com.fecaf.model.Cliente;
+
 public class Principal {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+		Scanner entradaDados = new Scanner(System.in);
+		Cliente cliente = new Cliente();
+		
+		System.out.println("Nome:");
+		cliente.setNome(entradaDados.next());
+		
+		System.out.println("Telefone:");
+		cliente.setTelefone(entradaDados.next());
+		
+		System.out.println("Celular:");
+		cliente.setCelular(entradaDados.next());
+		
+		System.out.println("Email:");
+		cliente.setEmail(entradaDados.next());
+		
+		//Metodo para inserir o novo Cliente
+		if (cliente.setNovoCliente(cliente))
+			System.out.println("Cliente cadastrado com sucesso!");
+		else
+			System.out.println("Registro não inserido no banco de dados!");
+		
+		
+		cliente.getListClientes();
+		entradaDados.close();
 
 	}
 
--- Aula12/src/br/com/fecaf/model/Cliente.java
+++ Aula12/src/br/com/fecaf/model/Cliente.java
@@ -1,5 +1,11 @@
 package br.com.fecaf.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import br.com.fecaf.repository.ConexaoMysql;
 
 public class Cliente {
@@ -58,15 +64,73 @@ public class Cliente {
 
 	//Metodos
 	//Insere um novo Cliente no Banco de Dados
-	public boolean setNovoCliente () {
-		//instancia
-		ConexaoMysql conexaoMysql = new ConexaoMysql();
+	public boolean setNovoCliente (Cliente novoCliente) throws ClassNotFoundException, SQLException {
+		boolean status;
+		
+		try {
+			//instancia da classe de conexao com o MySQL
+			ConexaoMysql conexaoMysql = new ConexaoMysql();
+			//Recebe a conexao que foi estabelecida com o BD
+			Connection conexao = conexaoMysql.getConnection();
+			
+			//Prepara o script SQL para ser enviado ao BD
+			PreparedStatement stmt = conexao.prepareStatement("insert into tbl_cliente "
+					+ "(nome, telefone, celular, email) values (?, ?, ?, ?)");
+			
+			//Validação dos valores antes de ser enviado para o BD
+			stmt.setString(1, novoCliente.getNome());
+			stmt.setString(2, novoCliente.getTelefone());
+			stmt.setString(3, novoCliente.getCelular());
+			stmt.setString(4, novoCliente.getEmail());
+			
+			//Executa o script SQL no BD
+			stmt.execute();
+			stmt.close();
+			status = true;
+			
+		} catch (Exception e) {
+			status = false;
+		}
+
 		//insert
-		return true;
+		return status;
 	}
 	
 	//Retorna os clientes do Banco de Dados
-	public void getListClientes () {
-		//select
+	public void getListClientes () throws ClassNotFoundException, SQLException {
+		//instancia da classe de conexao com o MySQL
+		ConexaoMysql conexaoMysql = new ConexaoMysql();
+		//Recebe a conexao que foi estabelecida com o BD
+		Connection conexao = conexaoMysql.getConnection();
+		
+		String sql = "select * from tbl_cliente order by id desc";
+		//Prepara o script SQL para ser enviado ao BD
+		Statement stmt = conexao.prepareStatement(sql);
+		
+		ResultSet rsClientes = stmt.executeQuery(sql);
+		
+		while(rsClientes.next()) {
+			System.out.println("########################################################");
+			System.out.println("ID:" + rsClientes.getInt("id"));
+			System.out.println("Nome do Cliente:" + rsClientes.getString("nome"));
+			System.out.println("Telefone do Cliente:" + rsClientes.getString("telefone"));
+			System.out.println("Celular do Cliente:" + rsClientes.getString("celular"));
+			System.out.println("Email do Cliente:" + rsClientes.getString("email"));
+			System.out.println("########################################################");
+			
+		}
+		
+		stmt.close();
+		
+		
+		
+		
 	}
 }
+
+
+
+
+
+
+
